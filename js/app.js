66abba10926677fac02deac2e3e426ef8ae913ba
sheylabre <sheylabrenaicha@hotmail.com>
2016-11-16 13:57:33 -0500

    music-ajax-final

--- js/app.js
+++ js/app.js
@@ -2,36 +2,60 @@ $(document).ready(function (){
 	$("#search-form button").click(function(event){
 		event.preventDefault();
 		var term = cleanTerm($("#search-form input").val());
-		var results = callItunesSearch(term, showResults, noResults, error);
-	});
+		var results = callItunesSearch(term, showResults);
+		});
 
-	function callItunesSearch(searchTerm, showResults, noResultsMessage, error){
+
+
+	function callItunesSearch(searchTerm, showResults){
 		startLoading();
 		// Pueden revisar el API en el link https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/
-		var root = 'https://itunes.apple.com/search?';
+		var root = "https://itunes.apple.com/search?";
 		$.ajax({
-		// completa el ajax aquí
-        });
-	}
+    		url : root,
+    		method : "GET",
+    		data : {
+    			term : searchTerm,
+    			limit : 5
+    		},
+    		success : function(data) {
+	    	    console.log("todo bien");
+	    	    showResults(JSON.parse(data));
+    		},
 
+    		error : function(data) {
+	    	    console.log(data.status);
+	    	    noResultsMessage();
+    		},
+
+    		complete : function(data) {
+    	    stopLoading();
+    		}
+		});
+	}
   	// Imprime los resultados
 	function showResults(data){
 		console.log(data);
 		data.results.map(function(cancion, index){
+			var audio = new Audio();
+			audio.src = cancion.previewUrl;
+			audio.controls = true;
 			var song  = document.createElement("div");
 			$(song).addClass("song");
-			// genera los elementos de cada canción aquí
+			var image = document.createElement("img");
+			$(image).attr("src",cancion.artworkUrl30);
+			$(song).append(audio)
+			$(song).append(image)
 			$("#search-results").append(song);
 		});
 	}
-  
+
  	function noResults(){
        	$("#search-results").html(song);
     }
   	function error(){console.log("Error")}
-	
+
 	function cleanTerm(term){
-        term = $.trim(term);
 		return term.replace(/s/g, "+");
 	}
 
@@ -47,5 +71,8 @@ $(document).ready(function (){
 	function stopLoading(){
 		$(".spinner").remove();
 	}
+
 });
+
+
 
